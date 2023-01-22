8b2162a64a6341846e1cd94eaff6538b66c64232
Edward <edwenschhof@gmail.com>
2023-01-22 17:27:14 -0500

    Finished problem 1

---
+++ problem1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define UPPER_LIMIT 1000
+
+int main()
+{
+	int sum = 0;
+	for(int i = 0; i < UPPER_LIMIT; i++)
+	{
+		if(i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	printf("SUM: %d\n", sum);
+	return 0;
+}
